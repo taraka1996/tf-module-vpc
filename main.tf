092ad933e80be925c63e84bbe4312fe047174651
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 23:40:27 +0530

    changes

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ resource "aws_route_table_association" "private-association" {
 
 resource "aws_route" "route" {
   route_table_id = var.default_route_table
-  destination_ipv6_cidr_block = var.vpc_cidr
+  destinatiom_cidr_block = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
 }  
