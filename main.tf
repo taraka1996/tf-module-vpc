db2b99687f33b1852380b490736103856d749454
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 23:01:07 +0530

    changes

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_route_table" "private-route-table" {
     for_each = var.private_subnets 
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-",each.value["name"])[1]}"].id
+        nat_gateway_id = aws_nat_gateway.nat-gateway["public-${split("-",each.value["name"])[1]}"].id
     }
      
     
