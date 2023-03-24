cba4903432c65d3757259f4a970edd78c9597c57
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 17:09:59 +0530

    changes

--- main.tf
+++ main.tf
@@ -19,3 +19,18 @@ resource "aws_subnet" "public_subnets" {
     )
     
 }
+
+## private subnets
+resource "aws_subnet" "private_subnets" {
+    vpc_id = aws_vpc.main.id
+
+    for_each = var.private_subnets
+    cidr_block = each.value["cidr_block"]
+    availability_zone = each.value["availability_zone"]
+    tags   = merge(
+     var.tags,
+     { Name = "${var.env}-${each.value["name"]}" }
+
+    )
+    
+}
