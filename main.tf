81dccb942fb1deffbf17fb725de091b54450464e
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 22:03:00 +0530

    changes

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnets" {
 
 ##internet gateway
 
-resource "aws-internet-gateway" "igw" {
+resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.main.id
 
     tags = merge(
