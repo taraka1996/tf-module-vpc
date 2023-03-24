c4ec6a98748a9dba81c7ec7cb2911811fea0e522
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 21:43:09 +0530

    changes

--- main.tf
+++ main.tf
@@ -30,10 +30,11 @@ resource "aws_route_table" "public-route-table" {
     )
  }
 
- #resource "aws_route_table_association" "public-association" {
-    #for_each = var.public_subnets
-    #subnet_id = aws_subnet.public_subnets[each.value["name"]].id
-    #route_table_id = aws_route_table.[each.value["name"]].id }
+ resource "aws_route_table_association" "public-association" {
+    for_each = var.public_subnets
+    subnet_id = lookup(aws_subnet.public_subnets, each.value["name"], null,"id", null)
+    route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+    }
 
 ## private subnets
 resource "aws_subnet" "private_subnets" {
