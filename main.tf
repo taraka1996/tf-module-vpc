7ad29a883124a54c64ab9179b7731c6dbbf09009
TARAK <tarakaramtirumala@gmail.com>
2023-03-24 23:33:16 +0530

    changes

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ resource "aws_vpc_peering_connection" "peer" {
     peer_vpc_id = var.default_vpc_id
     vpc_id = aws_vpc.main.id
     auto_accept = true
+    tags   = merge(
+     var.tags,
+     { Name = "${var.env}-peer" }
+    )
 }
 
 
@@ -69,6 +73,11 @@ resource "aws_route_table" "public-route-table" {
         gateway_id = aws_internet_gateway.igw.id
     }
     
+    route {
+        cidr_block = data.aws_vpc.default_vpc.cidr_block
+        vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    }
+    
     for_each = var.public_subnets
     tags = merge(
         var.tags, 
@@ -109,7 +118,12 @@ resource "aws_route_table" "private-route-table" {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat-gateway["public-${split("-",each.value["name"])[1]}"].id
     }
-     
+    
+    route {
+        cidr_block = data.aws_vpc.default_vpc.cidr_block
+        vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    }
+    
     
     tags = merge(
         var.tags, 
